ca219f4672f9f3ea6f6d7850dc0e0eb8b7efffea
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-20 14:02:13 +0530

    terraform groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,36 +1,38 @@
-pipeline {
+def call() {
+    pipeline {
 
-    agent {
-        node {
-            label 'workstation'
-        }
-    }
-
-    parameters {
-        choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
-        choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick action')
-    }
-
-    stages {
-
-        stage('Terraform INIT') {
-            steps {
-                sh 'terraform init -backend-config=env-${env}/state.tfvars'
+        agent {
+            node {
+                label 'workstation'
             }
         }
 
-        stage('Terraform Apply') {
-            steps {
-                sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+            choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick action')
+        }
+
+        stages {
+
+            stage('Terraform INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+
+            stage('Terraform Apply') {
+                steps {
+                    sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
             }
         }
 
     }
-
-    post {
-        always {
-            cleanWs()
-        }
-    }
-
 }
