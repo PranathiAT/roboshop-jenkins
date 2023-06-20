0d6da5e0b31cff714d6f85e5d15df7e22007d207
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-20 13:59:31 +0530

    library

---
+++ vars/terraform.groovy
@@ -0,0 +1,36 @@
+pipeline {
+
+    agent {
+        node {
+            label 'workstation'
+        }
+    }
+
+    parameters {
+        choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+        choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick action')
+    }
+
+    stages {
+
+        stage('Terraform INIT') {
+            steps {
+                sh 'terraform init -backend-config=env-${env}/state.tfvars'
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
+            }
+        }
+
+    }
+
+    post {
+        always {
+            cleanWs()
+        }
+    }
+
+}
