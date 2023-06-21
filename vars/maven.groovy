6a93e019115535c03981a257f4f32d36ded024d5
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-21 10:19:07 +0530

    sonar scanner in groovy file/pipeline

---
+++ vars/maven.groovy
@@ -0,0 +1,63 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+        options{
+            ansiColor('xterm')
+        }
+
+
+
+        stages {
+            stage('Code Compile') {
+                steps {
+                    sh 'echo Code Compile'
+
+                }
+            }
+            stage('Code Quality') {
+                steps {
+                    //sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.8.27:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    sh 'echo Code Quality'
+                }
+            }
+
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit tests'
+                    //sh 'npm test'
+                }
+            }
+
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo Checkmarx Scan'
+                }
+            }
+
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo Checkmarx SCA Scan'
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
