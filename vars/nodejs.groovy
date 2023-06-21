bff3de36c3b6d3aba2c65b28f028111231d1a272
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-21 12:13:29 +0530

    unit test cases for nodejs

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -15,16 +15,15 @@ def call() {
         stages {
             stage('Code Quality') {
                 steps {
-                    sh 'ls -l'
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.94.107:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    //sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.94.107:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
 
                 }
             }
 
             stage('Unit Test Cases') {
                 steps {
-                    sh 'echo Unit tests'
-                    //sh 'npm test'
+                    //sh 'echo Unit tests'
+                    sh 'npm test'
                 }
             }
 
