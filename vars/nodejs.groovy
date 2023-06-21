2ebeb12b5e3878d220b8be12ae5485c397f5a5a0
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-21 13:14:03 +0530

    groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -23,7 +23,7 @@ def call() {
             stage('Unit Test Cases') {
                 steps {
                     //sh 'echo Unit tests'
-                    sh 'npm test'
+                    //sh 'npm test'
                 }
             }
 
