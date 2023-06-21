e57dfb56bb49c010c368050d999ba63fecd8bc00
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-21 11:41:34 +0530

    python groovy for payment

--- vars/python.groovy
+++ vars/python.groovy
@@ -13,7 +13,9 @@ def call() {
         stages {
             stage('Code Quality') {
                 steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.94.107:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                    sh 'ls -l'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.94.107:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+
                 }
             }
 
