1c26c552fb8dfd64e7399b91ce3b448c75099e72
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-21 11:13:00 +0530

    sonar quality gate

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -16,7 +16,7 @@ def call() {
             stage('Code Quality') {
                 steps {
                     sh 'ls -l'
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.94.107:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonarqualitygate.wait=true'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.94.107:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
 
                 }
             }
